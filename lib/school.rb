88d78fa1be5d63fb4b0bf6069e0b64fbbb014f75
meggs1 <megan.schertz@gmail.com>
2020-11-04 16:59:10 -0600

    commit tree dc22235e69860e957d54e7f0b029b7a47625021c

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+class School
+    attr_accessor :name, :roster, :grade
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if roster[grade]
+            roster[grade] << name
+        else
+            roster[grade] = []
+            roster[grade] << name 
+        end
+    end
+
+    def grade(num)
+        roster.collect do |grade, name|
+            if grade == num
+                return name
+            end
+        end
+    end
+
+    def sort
+        sorted_roster = {}
+        roster.each do |grade, name|
+            sorted_roster[grade] = name.sort
+        end
+        sorted_roster
+    end
+
+
+
+end
